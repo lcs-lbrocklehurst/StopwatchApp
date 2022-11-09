9638a630ccab7f96fef0469012dfdba750a9db71
lbrocklehurst <lbrocklehurst@lcs.on.ca>
2022-11-09 09:09:07 -0500

    Initial Commit

---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by Lewis Brocklehurst on 2022-11-09.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
